91e3ce9e5a4facaf9148c2fd77e985f7a374a8c6
Steve Pousty <spousty@localhost.localdomain>
2017-03-08 16:18:10 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+
+
+
+
+
+namespace WebCore.netsample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+
+            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Program>()
+                .UseUrls(url)
+                .Build();
+
+            host.Run();
+
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvcWithDefaultRoute();
+        }
+
+    }
+}
